ed8ee9f32c9bc9fdb666dbeff61107ffa80987c3
Niourf <niourf@gmail.com>
2013-02-21 13:25:23 +0100

    put regs variable in core.c instead of core.h modified initCore function

--- src/core.c
+++ src/core.c
@@ -3,6 +3,21 @@
 
 #include "core.h"
 
+struct {
+  uint16_t A; /* General registers */
+  uint16_t B;
+  uint16_t C;
+  uint16_t X;
+  uint16_t Y;
+  uint16_t Z;
+  uint16_t I;
+  uint16_t J;
+  uint16_t PC; /* Program counter */
+  uint16_t SP; /* Stack pointer */
+  uint16_t EX; /* Extra/excess register */
+  uint16_t IA; /* Interrupt address */
+} regs;
+
 void initCore() {
-  memset(&regs, 0, 8 * sizeof(uint16_t));
+  memset(&regs, 0, 12 * sizeof(uint16_t));
 }
--- src/core.h
+++ src/core.h
@@ -11,19 +11,4 @@
 
 void initCore();
 
-struct {
-  uint16_t A; /* General registers */
-  uint16_t B;
-  uint16_t C;
-  uint16_t X;
-  uint16_t Y;
-  uint16_t Z;
-  uint16_t I;
-  uint16_t J;
-  uint16_t PC; /* Program counter */
-  uint16_t SP; /* Stack pointer */
-  uint16_t EX; /* Extra/excess register */
-  uint16_t IA; /* Interrupt address */
-} regs;
-
 #endif
